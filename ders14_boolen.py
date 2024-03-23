5605d757b1313c7d39fd17cf2eb5511bf925c34a
= <=>
2024-03-23 21:53:03 +0300

    14-15-16. dersler girildi

---
+++ ders14_boolen.py
@@ -0,0 +1,42 @@
+#boolean veri tipleri
+"""
+bool1 = True
+bool2 = False
+
+print(type(bool1))
+print(type(bool1))
+"""
+"""
+a = 2
+b = True
+
+c = 0
+d = False
+
+print(a>b)
+print(c == d)
+"""
+
+"""
+age = 32
+bool3 = age >= 26
+
+print(bool3)
+"""
+
+"""
+print(3 < 5)
+
+print('5' != 5)
+"""
+
+"""
+a = ['ahmet', 'mehmet']
+b = ['ahmet', 'mehmet']
+
+print(a==b) # True verir
+print(a is b) # False verir
+"""
+
+print(32 > 28 and 17 > 14)# false verir
+print(not False)# True döner
---
+++ ders15_if_elif_else.py
@@ -0,0 +1,55 @@
+"""
+age = 14 
+
+if age >= 18:
+    print('Oy kullanabilirsiniz')
+"""
+
+"""
+my_list = ['ahmet', 'mehmet', 'ebru']
+
+if 'ahmet' in my_list:
+    print(' Ahmet listede var')
+
+"""
+
+"""
+age = 15
+
+if age >= 18:
+    print('oy kullanabilirsiniz')
+else:
+    print('oy kullanamazsınız')
+
+"""
+
+"""
+age = 15 
+
+if age <= 2:
+    print('%80 indirimli')
+elif age <= 10: 
+    print('%50 indirimli')
+else:
+    print('indirim yok')
+
+"""
+
+"""
+age = int(input('lütfen yaşınızı giriniz: '))
+
+if age < 0:
+    age = 0
+    print('negatif sayı sıfıra çevirildi')
+
+elif age == 0:
+    print('lütfen 0 dan başka bir sayı giriniz')
+
+elif age < 18:
+    print('oy kullanamazsınız')
+
+else:
+    print('oy kullanabilirsiniz')
+
+"""
+
